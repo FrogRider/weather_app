8afabc8c790a0e99e57ee2c1a2457620a3f4f849
Jekos <jhonny.lbk@gmail.com>
2019-10-28 15:10:36 +0200

    Updates

---
+++ precache-manifest.f3dab02d42996d02cb4ba13b1e057ff1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32f892a3cf433ac8de6aff9c23d0567c",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "97e4e9202b7c2a708d3d",
+    "url": "/weather_app/static/css/main.4c88c3e6.chunk.css"
+  },
+  {
+    "revision": "fd0be22b7f45cfa3e1e2",
+    "url": "/weather_app/static/js/2.82598e87.chunk.js"
+  },
+  {
+    "revision": "97e4e9202b7c2a708d3d",
+    "url": "/weather_app/static/js/main.54cba847.chunk.js"
+  },
+  {
+    "revision": "23fd1cd65a8563fa95bd",
+    "url": "/weather_app/static/js/runtime~main.17b50636.js"
+  }
+]);
